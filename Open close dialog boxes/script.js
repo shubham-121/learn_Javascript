c6d74d034c7f9a26b86d6e43646c52dbf142212f
Shubham Bhatt <81768689+shubham-121@users.noreply.github.com>
2024-02-17 14:19:40 +0530

    Add files via upload

---
+++ Open close dialog boxes/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  //console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', () => {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+function closeModal() {
+  console.log('clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//***same functionality with redudant code */
+// btnCloseModal.addEventListener('click', () => {
+//   console.log('clicked');
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+// overlay.addEventListener('click', () => {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
