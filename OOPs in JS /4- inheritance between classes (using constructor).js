3182f6f09e1d4ea8d6256941d6e3208349460a4a
Shubham Bhatt <81768689+shubham-121@users.noreply.github.com>
2024-04-30 14:10:03 +0530

    Create 4- inheritance between classes (using constructor).js
    inheritance between classes (using constructor). Manually linking the prototypes of 2 functions

---
+++ OOPs in JS /4- inheritance between classes (using constructor).js
@@ -0,0 +1,32 @@
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  // this.firstName = firstName;
+  // this.birthYear = birthYear;
+  // Person(firstName, birthYear); //regular func call 'this' is set to undefinde. So we also set this keyword using call( func)
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+#//linking the prototype manually
+Student.prototype = Object.create(Person.prototype); //student.prototype inherits from person.prototype
+#
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I studey in ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+mike.calcAge();
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
